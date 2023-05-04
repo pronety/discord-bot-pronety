61af3c8221d0e7b2671d8228ea166b368ab8cfec
Andndre <agungandre687@gmail.com>
2023-05-04 22:00:29 +0800

    remove prisma & supabase

--- web/prisma/migrations/20230420044722_init/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Verifying" (
-    "key" TEXT NOT NULL,
-    "email" TEXT NOT NULL,
-    "discord_id" TEXT NOT NULL,
-
-    CONSTRAINT "Verifying_pkey" PRIMARY KEY ("key")
-);
